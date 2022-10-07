b70d171c60e7425e0517e9a81ab399da0f49832b
Tonyginto <antik998@mail.ru>
2022-10-07 22:14:58 +0300

    added dictionary of operators

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import kivy
 import re
+import sqlite3 as sl
 
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.textinput import TextInput
+
 
 # [^*/+-]{1}
 # [^0-9]
@@ -17,6 +19,7 @@ class IntInput(TextInput):
     # pat = re.compile('[0-9]|[\-]+|[\+]+')
 
     f = 0
+
     def replacing(self, matchobj):
         nm = [str(i) for i in range(10)]
         if self.f == 1 and matchobj.group(0)[0] not in nm:
@@ -57,36 +60,47 @@ class IntInput(TextInput):
 
         # s = re.sub(self.pat, '', substring)
 
-        s = re.sub('[0-9]|[\-]+|[\+]+', self.replacing, re.sub('[^0-9+-/*]','',substring))
+        s = re.sub('[0-9]|[\-]+|[\+]+', self.replacing, re.sub('[^0-9+-/*]', '', substring))
 
         return super().insert_text(s, from_undo=from_undo)
 
+
 class MyApp(App):
     total_sum = 0
+    dict_o = {'+': lambda x, y: x + y, '-': lambda x, y: x - y,
+              '*': lambda x, y: x * y, '/': lambda x, y: x // y}
 
     def build(self):
         # main_layout = BoxLayout(orientation='vertical')
         # main_layout = GridLayout(rows =2)
         main_layout = FloatLayout()
 
-        self.label_total = Label(text='Current total sum = '+str(self.total_sum),
-                            size_hint=(1., .05),
-                            pos_hint={'center_x': .5, 'center_y': .475}
-                            )
+        ht = 1
+        shy_i = .25  # размер input по y
+        cy_i = ht - shy_i / 2  # центр позиционирования input по y
         self.input = IntInput(
             multiline=True, readonly=False, halign="right", font_size=20,
-            size_hint=(1., .25),
-            pos_hint={'center_x': .5, 'center_y': .875}
+            size_hint=(1., shy_i),
+            pos_hint={'center_x': .5, 'center_y': cy_i}
         )
+        shy_s = .25  # размер solution по y
+        cy_s = cy_i - shy_i / 2 - shy_s / 2  # центр позиционирования solution по y
         self.solution = IntInput(
             multiline=True, readonly=True, halign="right", font_size=20,
-            size_hint=(1., .25),
-            pos_hint={'center_x': .5, 'center_y': .625}
+            size_hint=(1., shy_s),
+            pos_hint={'center_x': .5, 'center_y': cy_s}
         )
+        shy_lt = .05  # размер label_total по y
+        cy_c = cy_s - shy_s / 2 - shy_lt / 2  # центр позиционирования label_total по y
+        self.label_total = Label(text='Current total sum = ' + str(self.total_sum),
+                                 font_size=20,
+                                 size_hint=(1., shy_lt),
+                                 pos_hint={'center_x': .5, 'center_y': cy_c}
+                                 )
         rbutton = Button(text='Run',
-                        size_hint=(.5, .25),
-                        pos_hint={'center_x': .25, 'center_y': .25}
-                        )
+                         size_hint=(.5, .25),
+                         pos_hint={'center_x': .25, 'center_y': .25}
+                         )
         cbutton = Button(text='Clear',
                          size_hint=(.5, .25),
                          pos_hint={'center_x': .75, 'center_y': 0.25}
@@ -105,38 +119,26 @@ class MyApp(App):
         return main_layout
 
     def on_change_label(self):
-        self.label_total.text = 'Current total sum = '+str(self.total_sum)
+        self.label_total.text = 'Current total sum = ' + str(self.total_sum)
         return
-
-    def choose_operator(self, op, a, b):
-        if op == '+':
-            ret = a+b
-            return a+b
-        if op == '-':
-            return a-b
-        if op == '*':
-            return a*b
-        if op == '/':
-            return a/b
 
     def on_press_buttonC(self, instance):
         self.total_sum = 0
-        self.solution.text = ''
-        self.input.text = ''
+        self.solution.text = self.input.text = ''
         self.on_change_label()
         return
 
     def on_press_button(self, instance):
         out = 0
         if self.input.text != '':
-            if self.input.text[-1] in ['+','-','*','/']:
+            if self.input.text[-1] in ['+', '-', '*', '/']:
                 self.input.text = self.input.text[:-1]
-            if self.input.text[0] in ['+','*','/']:
+            if self.input.text[0] in ['+', '*', '/']:
                 self.input.text = self.input.text[1:]
             nums = re.findall('[0-9]+', self.input.text)
             operators = re.findall('[^0-9]', self.input.text)
             if self.input.text[0] == '-':
-                nums[0] = str(int(nums[0])*(-1))
+                nums[0] = str(int(nums[0]) * (-1))
                 operators.pop(0)
                 self.solution.text = str(self.total_sum) + self.input.text
                 self.input.text = self.input.text[1:]
@@ -144,22 +146,21 @@ class MyApp(App):
                 self.solution.text = str(self.total_sum) + '+' + self.input.text
             out = int(nums[0])
             for i in range(len(operators)):
-                a = out
-                b = int(nums[i+1])
-                out = self.choose_operator(operators[i], a, b)
+                out = self.dict_o[operators[i]](out, int(nums[i + 1]))
 
         #     print(kivy.app.App.user_data_dir)
-
-        # fname = str(kivy.app.App.user_data_dir) + '/save_file.txt'
+        # str(kivy.app.App.user_data_dir)
+        # fname =  'E/My android apps'+ '/save_file.txt'
         # f = open(fname, 'r+')
         # f.write(str(out))
         # f.close()
 
         self.total_sum += out
-        self.solution.text += '='+str(self.total_sum)
+        self.solution.text += '=' + str(self.total_sum)
         self.input.text = ''
         self.on_change_label()
         return
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
